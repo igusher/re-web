c98edcd0932e7fb0c404087f6e6b33d1b5358a74
Ievgen Gusher <i.gusher@samsung.com>
2013-09-23 06:44:38 +0000

    fix submitTrxs.py. add logs

--- pyth_client/submitTrxs.py
+++ pyth_client/submitTrxs.py
@@ -4,11 +4,12 @@ import datetime
 
 fileList = [sys.argv[i] for i in range(1,len(sys.argv))]
 numOfTrxsInRequest = 1000
-url = "http://127.0.0.1:8080/motm/motm/re/submitTrxs"
+#url = "http://127.0.0.1:8080/motm/motm/re/submitTrxs"
+url = "http://ec2-54-229-186-249.eu-west-1.compute.amazonaws.com:8080/motm/motm/re/submitTrxs"
 headers = {"content-type":"text/plain"}
 payload = "249-345;01199-01199;2013-05-03;000001;1522"
 
-
+log = open("/home/user/redemptionengine/submitTrxs.log","w")
 for trxFilePath in fileList:
 	trxFile = open(trxFilePath, 'r');
 	requestBody = ""
@@ -19,7 +20,13 @@ for trxFilePath in fileList:
 		if (trxId % numOfTrxsInRequest) == 0:
 			res = requests.post(url, data=requestBody, headers=headers);
 			successCount += int(res.text);
+			log.write(requestBody);
 			requestBody =""
+	if (requestBody != ""):
+		res= requests.post(url,data=requestBody,headers=headers);
+		successCount += int(res.text);
+		log.write(requestBody);
+		requestBody =""
 	finish = datetime.datetime.now();
 	print start,finish;
 	print trxFilePath, successCount;
